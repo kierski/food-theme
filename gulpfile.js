24a40108f9c30771505347d1cf897dcde8dd355c
Andrzej <kierski.andrzej@gmail.com>
2017-09-22 06:40:49 +0200

    git init

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
+const babel = require('gulp-babel');
+
+gulp.task('default', () => {
+	return gulp.src(['src/js/load.js', 'src/js/header.js', 'src/js/main.js'])
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('public/js'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/js/**/*.js', ['default']);
+});
+
+gulp.task('components', () => {
+	return gulp.src(['src/components/jQuery/dist/jquery.min.js', 'src/components/typed.js/js/typed.js'])
+    .pipe(plumber())
+    .pipe(concat('components.js'))
+    .pipe(uglify())
+		.pipe(gulp.dest('public/js'));
+});
---
+++ src/js/header.js
@@ -0,0 +1,4 @@
+
+const header = () => {
+  console.log('header');
+};
